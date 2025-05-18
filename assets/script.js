24fb86e83e70d9fa7f8432d8be1e061011d71d7f
Jan Wennrich <42703348+JanWennrich@users.noreply.github.com>
2025-05-18 23:51:34 +0200

    refactor: identify tabs by ids

--- assets/script.js
+++ assets/script.js
@@ -4,15 +4,15 @@
     const tabSwitches = document.querySelectorAll('.tab-switch > input[type=radio]');
     const tabPanels = document.querySelectorAll('.tab-panel');
 
-    tabSwitches.forEach((tabSwitch, tabSwitchIndex) => {
+    tabSwitches.forEach((tabSwitch) => {
         tabSwitch.addEventListener('change', () => {
-            tabPanels.forEach((tabPanel, tabPanelIndex) => {
-                if (tabSwitchIndex === tabPanelIndex) {
-                    tabPanel.style.display = 'block';
-                } else {
-                    tabPanel.style.display = 'none';
-                }
+            const controlledTabPanel = document.getElementById(tabSwitch.getAttribute('aria-controls'));
+
+            tabPanels.forEach((tabPanel) => {
+                tabPanel.style.display = 'none';
             });
+
+            controlledTabPanel.style.display = 'block';
         });
     });
 })();
